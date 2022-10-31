823198c23b59ec0e3d360387299f2db1b00e014c
itschrisagain <chris.achinga@student.moringaschool.com>
2022-10-31 10:14:26 +0300

    fetch list

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 import React, { useEffect, useState } from 'react'
-import { ActivityIndicator, FlatList, Text, View } from 'react-native'
-
-export default function App() {
-  const [isLoading, setLoading] = useState(true)
-  const [data, setData] = useState([])  
- 
-
+import { View, Text, ActivityIndicator, FlatList } from 'react-native'
+export default function Books() {
+  const [isLoading, setIsLoading] = useState([])
+  const [data, setData] = useState(true)
+  const [usersData, setUsersData] = useState([])
+  const getData = () => {
+    fetch('https://fudap-books-api.herokuapp.com/books')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+    setIsLoading(false)
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+  const renderItem = ({ item }) => {
+    return <Text>{item.title}</Text>
+  }
   return (
-    <View style={{ flex: 1, padding: 24 }}>
-    </View>)
- 
+    <View>
+      {isLoading && <ActivityIndicator size={38} />}
+      {data && (
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      )}
+    </View>
+  )
 }
-
